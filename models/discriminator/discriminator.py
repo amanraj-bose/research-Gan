853850ab3b030df8d9d2cd241220ec7d83690866
Aman Raj <88227750+amanraj-bose@users.noreply.github.com>
2024-03-25 14:57:17 +0530

    Update discriminator.py

--- models/discriminator/discriminator.py
+++ models/discriminator/discriminator.py
@@ -27,7 +27,7 @@ from keras.models import (
 init = tf.random_normal_initializer(0.0, 0.035)
 
 PipeLine = Sequential([
-    Conv2D(256, (4, 4), padding="same", use_bias=False, kernel_initializer=init),
+    Conv2D(256, (5, 5), padding="same", use_bias=False, kernel_initializer=init),
     BatchNormalization(),
     LeakyReLU(0.2)
 ])
@@ -44,35 +44,35 @@ def Discriminator(shape:tuple) -> Model:
     distance = Concatenate()([truth, predicted])
 
     # Extend Block Mechanism
-    x = CNNBLock(256, (4, 4))(distance)
-    x = CNNBLock(256, (4, 4))(x)
+    x = CNNBLock(256, (3, 3))(distance)
+    x = CNNBLock(256, (3, 3))(x)
     x_SE_1= SEBlock(256)(x)
-    x = CNNBLock(256, (4, 4))(x) + x_SE_1
+    x = CNNBLock(256, (3, 3))(x) + x_SE_1
     x = MaxPooling2D()(x)
 
     # Extend Block Mechanism - 2
-    x = CNNBLock(128, (4, 4))(x)
-    x = CNNBLock(128, (4, 4))(x)
+    x = CNNBLock(128, (3, 3))(x)
+    x = CNNBLock(128, (3, 3))(x)
     x_SE_2= SEBlock(128)(x)
-    x = CNNBLock(128, (4, 4))(x) + x_SE_2
+    x = CNNBLock(128, (3, 3))(x) + x_SE_2
     x = BatchNormalization()(x)
     x = MaxPooling2D()(x)
 
     # Attention Mechanism
-    x = CNNBLock(128, (4, 4))(x)
-    x = CNNBLock(64, (4, 4))(x)
+    x = CNNBLock(128, (3, 3))(x)
+    x = CNNBLock(64, (3, 3))(x)
     x_SE_2= ChannelSpatialAttention(64, 64)(x)
-    x = CNNBLock(64, (4, 4))(x_SE_2)
+    x = CNNBLock(64, (3, 3))(x_SE_2)
     x = BatchNormalization()(x)
-    x = Conv2D(128, (4, 4), padding="same", use_bias=False, kernel_initializer=init, activation=LeakyReLU(0.2))(x)
+    x = Conv2D(128, (3, 3), padding="same", use_bias=False, kernel_initializer=init, activation=LeakyReLU(0.2))(x)
     x = BatchNormalization()(x)
 
     # Output Block
-    x = Conv2D(64, (4, 4), padding="same", use_bias=False, kernel_initializer=init, activation=LeakyReLU(0.2))(x)
+    x = Conv2D(64, (3, 3), padding="same", use_bias=False, kernel_initializer=init, activation=LeakyReLU(0.2))(x)
     x = GlobalAveragePooling2D()(x)
     x = Flatten()(x)
-    x = Dense(1024, LeakyReLU(0.2), use_bias=False,kernel_initializer=init)(x)
-    x = Dense(512, LeakyReLU(0.2), use_bias=False, kernel_initializer=init)(x)
+    x = Dense(512, LeakyReLU(0.2), use_bias=False,kernel_initializer=init)(x)
+    x = Dense(128, LeakyReLU(0.2), use_bias=False, kernel_initializer=init)(x)
     x = Dense(1, "sigmoid", use_bias=True, kernel_initializer=init)(x)
 
     return Model([input_truth, input_predicted], x)
