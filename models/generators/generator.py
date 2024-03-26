17e029b0191c5f0ffb7991da174b859aff735587
Aman Raj <88227750+amanraj-bose@users.noreply.github.com>
2024-03-26 23:23:30 +0530

    Update generator.py

--- models/generators/generator.py
+++ models/generators/generator.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
 from keras.layers import (
-    LeakyReLU,
     Input,
     BatchNormalization,
     Dense,
-    Conv2DTranspose
+    Conv2DTranspose,
+    Dropout
 )
 from .layers import (
     DenoiseConvolution2D,
     EncoderBlock,
     PixelNormalization2D,
     LeakyReLU,
-    DecoderBlock
+    DecoderBlock,
 )
 
 from keras.models import Model, Sequential
@@ -24,6 +24,16 @@ def Generator(shape:tuple, k_size:tuple=(4, 4)) -> Model:
     inputs = Input(shape)
     init = tf.random_normal_initializer(0., 0.02)
 
+    # Sequential Noisy Blocks
+    noise = Sequential([
+        PixelNormalization2D(),
+        Dense(512, LeakyReLU(), use_bias=False, kernel_initializer=init),
+        Dense(512, LeakyReLU(), use_bias=False, kernel_initializer=init),
+        Dense(512, LeakyReLU(), use_bias=False, kernel_initializer=init),
+        Dense(512, LeakyReLU(), use_bias=False, kernel_initializer=init),
+        Dropout(0.5)
+    ], name="styles")(inputs)
+
     # Encoder Section
     Encoder_1 = EncoderBlock(64, k_size, init, True)(inputs)
     Encoder_2 = EncoderBlock(128, k_size, init)(Encoder_1)
@@ -35,14 +45,17 @@ def Generator(shape:tuple, k_size:tuple=(4, 4)) -> Model:
     
     # Decoder Section
     Decoder_1 = DecoderBlock(512, k_size, init, True)(Encoder_7, None, None)
-    Decoder_2 = DecoderBlock(512, k_size, init, True)(Decoder_1, Encoder_5, None)
-    Decoder_3 = DecoderBlock(512, k_size, init, True)(Decoder_2, Encoder_4, None)
-    Decoder_4 = DecoderBlock(512, k_size, init, True)(Decoder_3, None, Encoder_3)
+    Decoder_2 = DecoderBlock(512, k_size, init, True)(Decoder_1, noise, Encoder_5)
+    Decoder_3 = DecoderBlock(512, k_size, init, True)(Decoder_2, noise, Encoder_4)
+    Decoder_4 = DecoderBlock(512, k_size, init, True)(Decoder_3, noise, Encoder_3)
 
     Decoder_5 = DecoderBlock(256, k_size, init, False)(Decoder_4, None, Encoder_2)
     Decoder_6 = DecoderBlock(128, k_size, init, False)(Decoder_5, None, Encoder_1)
+
+    x = tf.nn.depth_to_space(Decoder_6, 2)
+    x = Conv2DTranspose(64, (4, 4), use_bias=False, padding="same", activation=LeakyReLU(0.2), kernel_initializer=init)(x)
     
-    outputs = Conv2DTranspose(3, (4, 4), padding="same", use_bias=True, activation="tanh", kernel_initializer=init, strides=(2, 2))(Decoder_6)
+    outputs = Conv2DTranspose(3, (4, 4), padding="same", use_bias=True, activation="tanh", kernel_initializer=init)(x)
 
     return Model(inputs, outputs)
 
