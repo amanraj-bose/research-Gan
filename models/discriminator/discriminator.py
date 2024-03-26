80ce469f1a86e70f12b565e85ae9ee6dc503d0b8
Aman Raj <88227750+amanraj-bose@users.noreply.github.com>
2024-03-26 23:06:41 +0530

    Update discriminator.py

--- models/discriminator/discriminator.py
+++ models/discriminator/discriminator.py
@@ -17,11 +17,11 @@ def downsample(filters, size, apply_batchnorm=True):
   return result
 
 
-def Discriminator():
+def Discriminator(shape:tuple):
   initializer = tf.random_normal_initializer(0., 0.02)
 
-  inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
-  tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
+  inp = tf.keras.layers.Input(shape=shape, name='input_image')
+  tar = tf.keras.layers.Input(shape=shape, name='target_image')
 
   x = tf.keras.layers.concatenate([inp, tar])  # (batch_size, 256, 256, channels*2)
 
