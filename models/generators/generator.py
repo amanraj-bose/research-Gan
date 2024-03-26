b9328dfe14ee87364cf47f5a58386000bd8665bf
Aman Raj <88227750+amanraj-bose@users.noreply.github.com>
2024-03-26 19:54:16 +0530

    Update generator.py

--- models/generators/generator.py
+++ models/generators/generator.py
@@ -1,111 +1,66 @@
 import tensorflow as tf
 from keras.layers import (
-    Conv2DTranspose,
     LeakyReLU,
-    Conv2D,
-    Flatten,
     Input,
     BatchNormalization,
-    Dropout,
-    Concatenate
+    Dense,
+    Conv2D
 )
 from .layers import (
-    AdaIN,
     DenoiseConvolution2D,
     EncoderBlock,
     PixelNormalization2D,
-    DecoderBlock,
-    MLP,
-    PixelShuffler,
-    LeakyReLU
+    LeakyReLU,
+    DecoderBlock
 )
 
 from keras.models import Model, Sequential
 from keras.utils import plot_model
 
 
-def Generator(inputs:tuple, dlayers:int=5, channels_in:int=3, out:int=3, denoised_perm:bool=False) -> Model:
-    input = Input(inputs)
-    init = tf.random_normal_initializer(0., 0.035)
-    denoised = input
-    if denoised_perm:
-        denoised = DenoiseConvolution2D(channels_in, dlayers)(input)
-    
-    # Encoder Block
-    encoder_1 = EncoderBlock(512, (5, 5), init)(denoised)
-    encoder_2 = EncoderBlock(512, (3, 3), init, False)(encoder_1)
-    encoder_3 = EncoderBlock(256, (3, 3), init, True)(encoder_2)
-    encoder_4 = EncoderBlock(128, (3, 3), init, True)(encoder_3)
-    encoder_5 = EncoderBlock(128, (3, 3), init, True)(encoder_4)
-    encoder_6 = EncoderBlock(64, (3, 3), init, False)(encoder_5)
-    encoder_7 = EncoderBlock(32, (3, 3), init, False)(encoder_6)
-    encoder_out = Conv2D(16, (3, 3), padding="same", use_bias=False, kernel_initializer=init, activation=LeakyReLU())(encoder_7)
 
-    # MLPs
-    W_affine_transform = Sequential([
+
+def Generator(shape:tuple, k_size:tuple=(5, 5)) -> Model:
+    inputs = Input(shape)
+    init = tf.random_normal_initializer(0., 0.05)
+
+    Noisy = Sequential([
         BatchNormalization(),
-        MLP(256, False),
-        MLP(256, False),
-        MLP(256, False),
-        PixelNormalization2D(),
-        MLP(256, False),
-        MLP(256, False),
-        MLP(256, False)
-    ], name="W-Noise")(input)
+        Dense(512, LeakyReLU(), use_bias=False, kernel_initializer=init),
+        Dense(512, LeakyReLU(), use_bias=False, kernel_initializer=init),
+        Dense(512, LeakyReLU(), use_bias=False, kernel_initializer=init),
+        PixelNormalization2D(1e-8)
+    ])(inputs)
 
-    # Decoder Block - 1
-    decoder_1 = DecoderBlock(256, (3, 3), init, False)(encoder_out)
-    AdaIN_1 = AdaIN()(content=decoder_1, style=W_affine_transform)
+    Styler_1 = Dense(256, LeakyReLU(), use_bias=False, kernel_initializer=init)(Noisy)
+    Styler_2 = Dense(128, LeakyReLU(), use_bias=False, kernel_initializer=init)(Styler_1)
 
-    # Decoder Block - 2
-    decoder_2 = DecoderBlock(256, (3, 3), init, False)(AdaIN_1)
-    AdaIN_2 = AdaIN()(content=decoder_2, style=W_affine_transform)
-    InConvolution_1 = Conv2D(128, (1, 1), padding="same", kernel_initializer=init, activation=LeakyReLU(), use_bias=False)(AdaIN_2)
-    Concatenate_1 = Concatenate()([InConvolution_1, encoder_5])
-    # Decoder Block - 3
-    decoder_3 = DecoderBlock(256, (3, 3), init, False)(Concatenate_1)
-    AdaIN_3 = AdaIN()(content=decoder_3, style=W_affine_transform)
-    InConvolution_2 = Conv2D(128, (1, 1), padding="same", kernel_initializer=init, activation=LeakyReLU(), use_bias=False)(AdaIN_3)
-    Concatenate_2 = Concatenate()([InConvolution_2, encoder_4])
+    # Encoder Section
+    Encoder_1 = EncoderBlock(64, k_size, init, True)(inputs)
+    Encoder_2 = EncoderBlock(128, k_size, init)(Encoder_1)
+    Encoder_3 = EncoderBlock(256, k_size, init)(Encoder_2)
+    Encoder_4 = EncoderBlock(512, k_size, init)(Encoder_3)
+    Encoder_5 = EncoderBlock(512, k_size, init)(Encoder_4)
+    Encoder_6 = EncoderBlock(512, k_size, init)(Encoder_5)
+    Encoder_7 = EncoderBlock(512, k_size, init)(Encoder_6)
+    
+    # Decoder Section
+    Decoder_1 = DecoderBlock(512, k_size, init, True)(Encoder_7, Noisy, Encoder_6)
+    Decoder_2 = DecoderBlock(512, k_size, init, True)(Decoder_1, Noisy, Encoder_5)
+    Decoder_3 = DecoderBlock(512, k_size, init, True)(Decoder_2, Noisy, Encoder_4)
+    Decoder_4 = DecoderBlock(512, k_size, init, True)(Decoder_3, Noisy, Encoder_3)
 
-    # Decoder Block - 4 
-    decoder_4 = DecoderBlock(256, (3, 3), init, False)(Concatenate_2)
-    AdaIN_4 = AdaIN()(content=decoder_4, style=W_affine_transform)
-    Concatenate_3 = Concatenate()([AdaIN_4, encoder_3])
+    Decoder_5 = DecoderBlock(256, k_size, init, False)(Decoder_4, Styler_1, Encoder_2)
+    Decoder_6 = DecoderBlock(128, k_size, init, False)(Decoder_5, Styler_2, Encoder_1)
+    
+    Decoder_7 = Conv2D(64, (9, 9), padding="same", kernel_initializer=init, use_bias=False)(Decoder_6)
+    Decoder_7 = tf.nn.depth_to_space(Decoder_7, 2)
+    Decoder_7 = tf.nn.leaky_relu(Decoder_7, 0.2)
 
-    # Decoder Block - 5
-    decoder_5 = DecoderBlock(256, (3,3), init, False)(Concatenate_3)
-    AdaIN_5 = AdaIN()(content=decoder_5, style=W_affine_transform)
-    InConvolution_4 = Conv2D(512, (1, 1), padding="same", kernel_initializer=init, activation=LeakyReLU(), use_bias=False)(AdaIN_5)
-    Concatenate_4 = Concatenate()([InConvolution_4, encoder_2])
+    outputs = Conv2D(3, (9, 9), padding="same", use_bias=True, activation="tanh", kernel_initializer=init)(Decoder_7)
 
-    # Decoder Block - 6
-    decoder_6 = DecoderBlock(256, (3, 3), init, False)(Concatenate_4)
-    AdaIN_6 = AdaIN()(content=decoder_6, style=W_affine_transform)
-    InConvolution_5 = Conv2D(512, (1, 1), padding="same", kernel_initializer=init, activation=LeakyReLU(), use_bias=False)(AdaIN_6)
-    Concatenate_5 = Concatenate()([InConvolution_5, encoder_1])
-
-    # Top of Decoder
-    decoder_7 = DecoderBlock(512, (3, 3), init)(Concatenate_5)
-
-    # Output
-    x = Conv2D(out, (5, 5), padding="same", activation="tanh", use_bias=True)(decoder_7)
-
-
-    return Model(input, x)
+    return Model(inputs, outputs)
 
 if __name__ == '__main__':
-    model = Generator((256, 256, 3))
-#     inp = (plt.imread(r"E:\keras\Research\GAN\models\generators\download.jpg"))
-#     inp = tf.image.resize(inp, (256, 256))
-#     gen_output = model(inp[tf.newaxis, ...], training=False)
-#     disc_out = Discriminator((256, 256, 3))([inp[tf.newaxis, ...], gen_output], training=False)
-#     # gen_output[0, ...]
-#     plt.imshow(disc_out[0, ..., -1], vmin=-20, vmax=20, cmap="RdBu_r")
-#     plt.colorbar()
-#     plt.show()
-
-    
-
-    plot_model(model, show_shapes=True)
-
+    x = Generator((256, 256, 3))
+    plot_model(x, show_shapes=True)
