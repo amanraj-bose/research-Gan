f91261c691a08a51d8798c79a6e71b9b30af0bf2
Aman Raj <88227750+amanraj-bose@users.noreply.github.com>
2024-03-28 14:33:23 +0530

    Update generator.py

--- models/generators/generator.py
+++ models/generators/generator.py
@@ -50,9 +50,9 @@ def Generator(shape:tuple, k_size:tuple) -> Model:
     Decoder_3 = DecoderBlock(512, k_size, init, True)(Decoder_2, noise, Encoder_4)
     Decoder_4 = DecoderBlock(512, k_size, init, True)(Decoder_3, noise, Encoder_3)
 
-    Decoder_5 = DecoderBlock(256, k_size, init, False)(Decoder_4, Encoder_2, None)
+    Decoder_5 = DecoderBlock(128, k_size, init, False)(Decoder_4, Encoder_2, None)
    
-    Decoder_6 = DecoderBlock(128, k_size, init, False)(Decoder_5, Encoder_1, None)
+    Decoder_6 = DecoderBlock(64, k_size, init, False)(Decoder_5, Encoder_1, None)
 
 
     x = Conv2DTranspose(64, k_size, use_bias=False, padding="same", activation=LeakyReLU(0.2), kernel_initializer=init, strides=(2,2))(Decoder_6)
